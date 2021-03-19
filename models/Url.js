9b28a12d85615559ff42e56bc0a4a555c275662b
Randika Rodrigo <randika.rodrigo@qmatic.com>
2021-03-19 14:28:41 +0530

    Added Url mongoose model.

---
+++ models/Url.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+	shortId: String,
+	longUrl: String,
+	shortUrl: String,
+	createdAt: { type: String, default: Date.now() }
+});
+
+module.exports = mongoose.model("Url", urlSchema);
